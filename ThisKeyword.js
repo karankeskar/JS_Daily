706dc8762ef6a96fdf93f448e5e1aef5c1f4761f
Karan D K <40066189+karankeskar@users.noreply.github.com>
2026-03-16 11:59:19 +1100

    This keywords in classes

--- ThisKeyword.js
+++ ThisKeyword.js
@@ -16,10 +16,75 @@
 
 // item.getItem();
 
+// Inside class this references to the instance of the class
+// class Item{
+//     title="Bali"
+//     getItem(){
+//         console.log("This", this);
+//     }
+// }
+
+// const item = new Item();
+// item.getItem()
+
+
+// Function inside class this is will return undefined as you specify the function keyword
+// class Item{
+//     title="Bali"
+//     getItem(){
+//         function someFun(){
+//             console.log("this", this)
+//         }
+//         someFun()
+//     }
+// }
+
+// const item = new Item();
+// item.getItem()
+
+
+// Map function inside getItem method inside the class, will again return undefined thrice as it is mapping over 3 elements in an array.
+
+// class Item{
+//     title="Bali"
+//     getItem(){
+//         [1,2,3].map(function (item){
+//             console.log("this", this)
+//         })
+//     }
+// }
+
+// const item = new Item();
+// item.getItem()
+
+
+
+// SOlution to using function keyword inside the class method
+
+// class Item{
+//     title="Bali"
+//     getItem(){
+//         const this_ = this;
+//         function someFun(){
+//             console.log("this", this_)
+//         }
+//         someFun()
+//     }
+// }
+
+// const item = new Item();
+// item.getItem()
+
+
+// Solution more simpler and newer to use by using arrow function
+
 class Item{
     title="Bali"
     getItem(){
-        console.log("This", this);
+        const someFun=()=>{
+            console.log("this", this)
+        }
+        someFun()
     }
 }
 
